0165ff69e92b91caa71f7e6f31367cc6268a3f84
Hanifan <gam2000e@gmail.com>
2026-08-23 12:48:52 +0700

    feat: add social links configuration data for hero section

--- src/features/hero/data/socialLinks.js
+++ src/features/hero/data/socialLinks.js
@@ -8,22 +8,22 @@ const socialLinks = [
   },
   {
     name: 'TikTok',
-    handle: 'haniiffan.png',
-    href: 'https://tiktok.com/@haniiffan.png',
+    handle: 'Haniffan.png',
+    href: 'https://www.tiktok.com/@hanippn',
     icon: 'https://cdn.jsdelivr.net/npm/simple-icons@11.15.0/icons/tiktok.svg',
     delay: 600
   },
   {
     name: 'GitHub',
     handle: 'Hanifan pangabekti',
-    href: 'https://github.com/Hanifan-pangabekti',
+    href: 'https://github.com/HanifanPNG',
     icon: 'https://cdn.jsdelivr.net/npm/simple-icons@11.15.0/icons/github.svg',
     delay: 700
   },
   {
     name: 'LinkedIn',
     handle: 'Hanifan pangabekti',
-    href: 'https://linkedin.com/in/Hanifan-pangabekti',
+    href: 'https://www.linkedin.com/in/hanifan-pangabekti-6b9b70265/',
     icon: 'https://cdn.jsdelivr.net/npm/simple-icons@11.15.0/icons/linkedin.svg',
     delay: 800
   }
